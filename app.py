4a6d187db8e9581e9aad9a8429d8548654ae9a2c
S.Yeon <angielsy424024@gmail.com>
2025-05-23 16:09:20 +0900

    initial commit

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 
 st.title("Rule")
-st.header("01. Sellect Target")
-st.text("You need to sellect a target in order to distroy your school.")
+st.header("01. Select Target")
+st.text("You need to select a target in order to destroy your school.")
 st.text("The more important your target is, the better.")
 st.header("02. Observe")
+st.text("Observe your target in order to find the day you can get access to your target's data.")
+st.header("03. Get Ready")
+st.text("Select the date you can destroy data without anyone knowing.")
+st.text("Find the way to get into your target's seat.")
+st.text("Prepare Virus that can destroy all the data, or some other way to physically destroy data.")
+st.header("04. Destroy")
+st.text("You're ready. Tools are set. Now, do your job.")
