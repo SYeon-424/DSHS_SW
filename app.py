6e344e6270cf33037e480bc57ef2962b3d136099
S.Yeon <angielsy424024@gmail.com>
2025-05-30 14:35:05 +0900

    initial commit

--- app.py
+++ app.py
@@ -14,3 +14,6 @@ st.text("Find the way to get into your target's seat.")
 st.text("Prepare Virus that can destroy all the data, or some other ways to physically destroy data.")
 st.header("04. Destroy")
 st.text("You're ready. Tools are set. Now, do your job.")
+
+
+st.text("은서는 하면 안됨 ^^")
